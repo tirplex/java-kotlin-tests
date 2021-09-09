1b259a974a843d158c3b6386c2455a33de2599bd
Egor Timokhov <timohovegor@gmail.com>
2021-09-09 21:31:56 +0300

    Add allure reports and configure screenshots
    Issue: None

--- src/test/java/base/TestsWatcherUI.java
+++ src/test/java/base/TestsWatcherUI.java
@@ -1,10 +1,16 @@
 package base;
 
 import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public class TestsWatcherUI implements TestWatcher, AfterAllCallback {
@@ -23,8 +29,18 @@ public class TestsWatcherUI implements TestWatcher, AfterAllCallback {
 
   @Override
   public void testFailed(ExtensionContext context, Throwable cause) {
-    //take custom screenshot and put in folder with test name
-    Selenide.screenshot("screenshots/" + context.getTestClass().get().getName() + "/" + context.getDisplayName());
+    // Take custom screenshot and put it in folder with test name
+    String testFolder = context.getTestClass().get().getName().replaceAll("\\.", "_");
+    String testName = context.getDisplayName().replaceAll("\\s+", "_");
+    String screenshot = Selenide.screenshot("screenshots/" + testFolder + "/" + testName);
+
+    // Add screenshot to allure report
+    Path content = Paths.get(screenshot.substring(5)); // remove file:/ prefix
+    try (InputStream is = Files.newInputStream(content)) {
+      Allure.addAttachment("Page screenshot", is);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
